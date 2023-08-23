fac94aeffa987547f5a6bb7b89211f9ffb85d27d
Iamtheusername112 <apkfem@gmail.com>
2023-08-23 13:09:28 +0200

    Type script Lessons

---
+++ app.ts
@@ -0,0 +1,12 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+// lets call the add function, pass in (number1, number2) as arguments and then store the funtion in a variable called result
+
+const result = add(number1, number2);
+
+console.log(result);
