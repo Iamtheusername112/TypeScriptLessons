948836ed02703e3af25f2305bab486d2fff861ad
Iamtheusername112 <apkfem@gmail.com>
2023-08-23 13:19:38 +0200

    Type script lessons

--- app.ts
+++ app.ts
@@ -1,11 +1,26 @@
-function add(n1, n2) {
+// REGULAR JAVASCRIPT
+
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// const number1 = 5;
+// const number2 = 2.8;
+
+// lets call the add function, pass in (number1, number2) as arguments and then store the funtion in a variable called result
+
+// const result = add(number1, number2);
+
+// console.log(result);
+
+// ------------------TYPESCRIPT FOR THE ABOVE JAVASCRIPT--------------------------------
+
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 2.8;
-
-// lets call the add function, pass in (number1, number2) as arguments and then store the funtion in a variable called result
 
 const result = add(number1, number2);
 
